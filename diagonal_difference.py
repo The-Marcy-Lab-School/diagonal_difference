59b7b47c423ecf9069efa4243711bc53f829fcf4
EC2 Default User <ec2-user@ip-172-31-24-82.us-east-2.compute.internal>
2020-01-08 15:35:15 +0000

    revisions

--- diagonal_difference.py
+++ diagonal_difference.py
@@ -1,26 +1,35 @@
-# P - Add the the right and left diagonals in a matrix then return the absolute value of the | left diagonal - right diagonal | 
+"""
+P - Add the the right and left diagonals in a matrix then return the absolute value of the | left diagonal - right diagonal | 
 
-# E - squarematrix = [[2, 3, 4, 5],
-#                 [5, 5, 5,10],
-#                 [0, 7, 13, 10],
-#                 [9, 9, 9, 9]]
+E - squarematrix = [[2, 3, 4, 5],
+                [5, 5, 5,10],
+                [0, 7, 13, 10],
+                [9, 9, 9, 9]]
 
-#     diagonal_matrix(squarematrix) == 3
+    diagonal_matrix(squarematrix) == 3
     
-#     squarematrix = []
+    squarematrix = []
     
-# D - For loops
+D - List Integers
 
-# A - For the left diagonal start at index 0 and then in go onto the next array and increment the index by 1 and continue adding the values until the last value of the last array is reached. 
-#   For the right diagonal start from the last index of the first array and go through each array and decrement by 1 until the last value is reached 
-#   return the abs value of the left diagonal - right diagonal
+A - 1. Declare two variables `ltr` - left to right, `rtl` - right to left 
+For the left diagonal start at index 0 and then in go onto the next array and increment the index by 1 and continue adding the values until the last value of the last array is reached. 
+  For the right diagonal start from the last index of the first array and go through each array and decrement by 1 until the last value is reached 
+  return the abs value of the left diagonal - right diagonal
+"""
 
-
-def diagonal_difference(squarematrix):
-    left_total = 0
-    right_total = 0 
-    for row in squarematrix:
-        for number in row:
-            left_total += number
-            row[number + 1]
+def diagonal_difference(matrix):
+    ltr, rtl = 0, 0
+    for i in range(len(matrix)):
+        ltr += matrix[i][i]
+        rtl += matrix[i][(len(matrix) - 1 )-  i]
         
+    return abs(ltr - rtl)
+    
+matrix = [[2, 3, 4, 5],
+        [5, 5, 5,10],
+        [0, 7, 13, 10],
+        [9, 9, 9, 9]]
+
+print(diagonal_difference(matrix));
+                
